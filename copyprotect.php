f2b16c059b731f991098834013a07f5931fb74f0
dragonzx <DragonZX@yandex.ru>
2016-12-01 20:59:14 +0300

    Options to separate file

--- copyprotect.php
+++ copyprotect.php
@@ -14,16 +14,7 @@ Copyright (C) 2010  Chetan Gole - chetangole.com
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+of the License, or (at your option) any later version. (see LICENSE)
 */
 
 // No right click (with message) - Problem for copy cats NO RIGHT CLICK
@@ -148,49 +139,7 @@ function CopyProtect_options_page()
 	$wp_CopyProtect_nrc = get_option('CopyProtect_nrc');
 	$wp_CopyProtect_nts = get_option('CopyProtect_nts');
 	?>
-	<form method="post" id="CopyProtect_options">
-		<fieldset class="options">
-		<table class="form-table">		
-			<tr valign="top"> 
-				<th width="33%" scope="row">Disable right mouse click:</th> 
-				<td>
-				<input type="radio" name="CopyProtect_nrc" value="0" <?php if($wp_CopyProtect_nrc == 0) echo('checked'); ?> />
-				Do not disable right click.<br />
-				<input type="radio" name="CopyProtect_nrc" value="1" <?php if($wp_CopyProtect_nrc == 1) echo('checked'); ?> />
-				Disable right click, no message.<br />
-				<input type="radio" name="CopyProtect_nrc" value="2" <?php if($wp_CopyProtect_nrc == 2) echo('checked'); ?> />
-				Disable right click and show message :
-				<input name="CopyProtect_nrc_text" type="text" id="CopyProtect_nrc_text" value="<?php echo get_option('CopyProtect_nrc_text') ;?>" size="30"/>
-				</td> 
-			</tr> 
-			<tr valign="top"> 
-				<th width="33%" scope="row">Disable text selection:</th> 
-				<td>
-				<input type="checkbox" id="CopyProtect_nts" name="CopyProtect_nts" value="CopyProtect_nts" <?php if($wp_CopyProtect_nts == true) { echo('checked="checked"'); } ?> />
-				Activate. <a href="http://www.thechetan.com/wp-copyprotect/#kp" target="_blank">Not working ?</a>
-				</td> 
-			</tr>	
-		<tr>
-        <th width="33%" scope="row">Save settings :</th> 
-        <td>
-		<input type="submit" name="CopyProtect_save" value="Save Settings" />
-        </td>
-        </tr>
-		<tr>
-        <th scope="row" style="text-align:right; vertical-align:top;">
-        <td>
-		<h3>Whats next ?</h3>
-		<p>Why don't you <a href="/wp-admin/post-new.php">write a post</a> about <a href="http://chetangole.com/blog/wp-copyprotect/" target="_blank">WP-CopyProtect</a> ?</p>
-		<h3>Problems, Questions, Suggestions ?</h3>
-		<p>Send me mail via: <a href="http://chetangole.com/blog/wp-copyprotect/" target="_blank">WP-CopyProtect Homepage</a></p>
-        </td>
-        </tr>
-		</table>
-		<h3>Thank you</h3>
-		Plug in developed by <a href="http://chetangole.com/blog/" target="_blank">Chetan Gole</a>. <br />
-		<small>Follow me on Twitter <a href="http://twitter.com/_techie" target="_blank">@_techie</a></small>
-		</fieldset>
-	</form>
+
 	</table>
 	</div>
 	<?php
